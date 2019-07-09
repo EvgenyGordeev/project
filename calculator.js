a199bd20663353c8a65f8ee224f73245281d3f44
EvgenyGordeev <fors137@rambler.ru>
2019-07-09 23:14:31 +0400

    calculate.js

---
+++ calculator.js
@@ -0,0 +1,29 @@
+let validnumber = true;
+
+ while (validnumber === true) {
+  let number1 = +prompt("Введите первое число", "");
+  let operator = prompt("Введите желаемую арифметическую операцию", "");
+  let number2 = +prompt("Введите второе число", "");
+
+
+function calculator(number1, operator, number2) {
+    
+  if (operator === "+") {
+      return number1 + number2;
+  } else if (operator === "-") {
+      return number1 - number2;
+  } else if (operator === "*") {
+      return number1 * number2;
+  } else if (operator === "/") {
+      return number1 / number2;
+  } else if (operator === "%") {
+	  return number1 % number2
+  } else if (operator === "^")
+      return Math.pow(number1,number2)
+}
+
+alert(number1 + String(operator) + number2 + '=' + calculator(number1, operator, number2));
+
+validnumber = confirm("Хотите выполнить еще вычисления ?");     
+     
+}
